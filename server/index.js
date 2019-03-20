f57323d9eef6c293b9e787c749b71dd2efab7bd2
Pankaj Goyal <pankaj.goyal@go-mmt.com>
2019-03-20 07:55:06 +0530

    first commit:

---
+++ server/index.js
@@ -0,0 +1,79 @@
+const keys = require('./keys');
+
+// express app setup
+
+const express = require('express');
+const bodyParser =  require('body-parser');
+const cors =  require('cors');
+
+const app = express();
+
+//cross version resource sharing
+app.use(cors());
+
+// parse request body to json
+app.use(bodyParser.json());
+
+
+// postgress client setup
+
+const {Pool} = require('pg');
+const pgClient = new Pool({
+    user:keys.pgUser,
+    password: keys.pgPassword,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    port: keys.pgPort
+});
+
+
+pgClient.on('error',() => console.log('Lost Pg connection'));
+
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
+
+// redis client
+
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
+
+app.get('/',(req,res) => {
+    res.send('Hi');
+});
+
+app.get('/values/all',async (req,res) => {
+    const values = pgClient.query('select * from values');
+    res.send(values.rows);
+});
+
+app.get('/values/current',async (req,res) => {
+    redisClient.hgetall('values',(err,values) => {
+        res.send(values);
+    });
+});
+
+
+app.post('/values', async (req,res) => {
+    const index  = req.body.index;
+    if(parseInt(index) > 40){
+        return res.status(422).send('index too high');
+    }
+
+    redisClient.hset('values',index,'Nothing Yet');
+    redisPublisher.publish('insert',index);
+
+    pgClient.query('INSERT INTO values(number) values ($1)',[index]);
+
+    res.send({working:true});
+});
+
+
+app.listen(5000,err => {
+    console.log('listening');
+});
